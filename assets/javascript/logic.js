984f53b7ae5d1d859d7b2ba7df7a97d61dc29ef7
Sara <bracewell.sara@gmail.com>
2018-01-11 12:56:52 -0600

    added moment.js and math to figure out the calcuations for the arrival of the next train and minutes to arrival, does not constantly update information and lacking details on refresh of page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,8 +16,9 @@ firebase.initializeApp(config);
   //not sure how to do time in javascript
   var time = 0;
   var frequency = 0;
-  var arrival = 0;
-  var minaway = 0;
+  var nextArrival = 0;
+  var minutesAway = "";
+  var firstTime = "";
 
 
   // Create a variable to reference the database
@@ -57,12 +58,11 @@ firebase.initializeApp(config);
     // Don't refresh the page!
     event.preventDefault();
 
-
     // Code in the logic for storing and retrieving the most recent train information
     train = $("#trainname-input").val().trim();
     destination = $("#destination-input").val().trim();
     frequency = $("#frequency-input").val().trim();
-    arrival = $("#arrivaltime-input").val().trim();
+    firstTime = $("#firsttime-input").val().trim();
     
     // Don't forget to provide initial data to your Firebase database. - set replaces old data
     //if you want to add more users than just the latest one, then use push
@@ -71,28 +71,81 @@ firebase.initializeApp(config);
       formtrain: train,
       formdestination: destination,
       formfrequency: frequency,
-      formarrival: arrival,
+      formfirsttime: firstTime,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
       //this is added so we can get most resent user so we can get most recent user to brower and to do this we need to change the listener  
     })
+
+    // Alert
+    alert("Train was successfully added");
+
+    // Clears all of the text-boxes
+    $("#trainname-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#firsttime-input").val("");
+
+    
       
   });
 
 
   // Firebase watcher + initial loader HINT: .on("value")
-  database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+  // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {  
+    //determine Next Arrival
+    // Assumptions
+    // var tFrequency = 3;
 
-    console.log(snapshot.val());
-    $("#trainname-display").text(snapshot.val().formtrain);
-    console.log(snapshot.val().formtrain);
-    $("#destination-display").text(snapshot.val().formdestination);
-    console.log(snapshot.val().formdestination);
-    $("#frequency-display").text(snapshot.val().formfrequency);
-    console.log(snapshot.val().formfrequency);
-    $("#arrivaltime-display").text(snapshot.val().formarrival);
-    console.log(snapshot.val().formarrival);
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log("Remainder: " + tRemainder);
+
+    // Minutes Away
+    var minutesAway = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+    // Next Train Arrival
+    var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm a"));
+
+    //determin Minutes Away
+
+    //want to push to table to add new train 
+    //add new table row
+    //add new train information into row
+    // Add each train's data into the table row
+  $("#train-table > tbody").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" +
+  frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+  // $("#trainname-display").append(snapshot.val().formtrain);
+  // $("#destination-display").append(snapshot.val().formdestination);
+  // $("#frequency-display").append(snapshot.val().formfrequency);
+  // $("#arrivaltime-display").append(nextArrival);
+  // $("#minaway-display").append(minutesAway);
+  console.log(childSnapshot.val().formtrain);
+  console.log(childSnapshot.val().formdestination);
+  console.log(childSnapshot.val().formfrequency);
+  console.log(nextArrival);
+  console.log(minutesAway);
     
   })
+
+  
 
   // Create Error Handling
 
